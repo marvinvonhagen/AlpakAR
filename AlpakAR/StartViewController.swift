0555c95a1bd97378f84ba3600aa97b6b282a923f
Marvin von Hagen <M@vHagen.me>
2018-05-22 14:20:15 +0200

    Ready for App Store

--- AlpakAR/StartViewController.swift
+++ AlpakAR/StartViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 import MessageUI
+import StoreKit
 
 class StartViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startRatingTimer()
 
         // Do any additional setup after loading the view.
     }
@@ -23,17 +25,40 @@ class StartViewController: UIViewController, MFMailComposeViewControllerDelegate
     }
     
     @IBOutlet var contactUS: UIButton!
-    
+    var ratingTimer: Timer!
+
     @IBAction func sendMail(_ sender: Any) {
         let mc: MFMailComposeViewController = MFMailComposeViewController()
         mc.mailComposeDelegate = self
         mc.setToRecipients(["COD3LTA@vHagen.me"])
         self.present(mc, animated: true, completion: nil)
+        ratingTimer.invalidate()
     }
     
-    
-    
+    @IBAction func cU(_ sender: Any) {
+        ratingTimer.invalidate()
 
+    }
+    @IBAction func SA(_ sender: Any) {
+        ratingTimer.invalidate()
+
+    }
+    
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
+    @objc func rR(_ timer: Timer) {
+        if #available(iOS 10.3, *) {
+            SKStoreReviewController.requestReview()
+        }
+    }
+    
+    func startRatingTimer() {
+        if #available(iOS 10.3, *) {
+            ratingTimer = Timer.scheduledTimer(timeInterval: 15, target: self, selector: #selector(rR(_:)), userInfo: nil, repeats: false)
+        }
+    }
     /*
     // MARK: - Navigation
 
--- AlpakAR/ViewController.swift
+++ AlpakAR/ViewController.swift
@@ -13,9 +13,10 @@ import ARKit
 class ViewController: UIViewController, ARSKViewDelegate {
     
     @IBOutlet var sceneView: ARSKView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -60,6 +61,31 @@ class ViewController: UIViewController, ARSKViewDelegate {
     
     // MARK: - ARSKViewDelegate
     
+
+    
+    
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+
+    @IBOutlet var sGR: UISwipeGestureRecognizer!
+    @IBAction func puSS(_ sender: Any) {
+        performSegue(withIdentifier: "uSSegue", sender: self)
+
+    }
+    
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         // Create and configure a node for the anchor added to the view's session.
         let rand = arc4random_uniform(_: 4)
@@ -88,20 +114,4 @@ class ViewController: UIViewController, ARSKViewDelegate {
         }
     }
     
-    
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
